1edbcc43a5f002415ef3e6e2278d5972490c8875
hepnerb <hepnerbryce@gmail.com>
2020-12-08 21:58:29 -0700

    darn about

--- front-end/newvue/src/router/index.js
+++ front-end/newvue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Admin from '../views/Admin.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 

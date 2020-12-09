0646beb44870c54a3483c18bdf9dc27e58d13d94
hepnerb <hepnerbryce@gmail.com>
2020-12-09 12:05:12 -0700

    fixing the header

--- front-end/newvue/src/App.vue
+++ front-end/newvue/src/App.vue
@@ -1,16 +1,27 @@
 <template>
 <div id="app">
-  <div class="header">
-    <router-link to="/">
-    
+  <!-- <div class="header"> -->
+    <!-- <router-link to="/"> -->
+    <div id="menu">
+
     <div id="brand">
       <router-link to="/">
         <img src="/images/logo.png">
       </router-link>
       <h1>Generic Used Car Site</h1>
     </div>
-    
-  </div>
+    </div>
+    <!-- <div class="header">
+    <router-link to="/">
+      <div class="logo">
+        <img src="/images/logo.png">
+      </div>
+    </router-link>
+    <div class="title">
+      <h1>Generic Used Car Site</h1>
+    </div>
+  </div> -->
+  <!-- </div> -->
   <div class="content">
     <router-view />
   </div>
@@ -27,6 +38,13 @@
 <style>
 html {
   box-sizing: border-box;
+}
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 5px;
+  grid-template-areas: "none brand side";
+  margin-bottom: 50px;
 }
 
 body {
